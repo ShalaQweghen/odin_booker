ad603885bbc3b6c0a6199a35c73e95b9d9c81c5e
Shala Qweghen <serafettinyilmaz04@gmail.com>
2016-09-07 13:50:39 +0300

    Add alert

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,12 +4,14 @@ class FlightsController < ApplicationController
 		@airports = Airport.all.map(&:code).uniq
 		@dates = Flight.all.map(&:date_formatted).uniq
 		unless params[:departure_id].nil?
-			if params[:departure_id] == params[:arrival_id]
-				flash.now[:alert] = "Your departure and arrival airports cannot be the same."
-			end
 			@flights = Flight.where(departure_id: Airport.find_by(code: params[:departure_id]).id,
 									arrival_id: Airport.find_by(code: params[:arrival_id]).id)
 			@flights = @flights.select { |flight| flight.date_formatted == params[:date] }
+			if params[:departure_id] == params[:arrival_id]
+				flash.now[:alert] = "Your departure and arrival airports cannot be the same."
+			elsif @flights.empty?
+				flash.now[:alert] = "No flights found matching your criteria."
+			end
 		end
 	end
 end
